9f55ec261b8436b0a98ad515a2bf91337a2b1337
Brad Hanks <hanks.brad@gmail.com>
2025-05-26 16:29:32 -0600

    big commit

---
+++ components/screenshot.tsx
@@ -0,0 +1,32 @@
+import { clsx } from 'clsx'
+import Image from 'next/image';
+
+export function Screenshot({
+  width,
+  height,
+  src,
+  className,
+}: {
+  width: number
+  height: number
+  src: string
+  className?: string
+}) {
+  return (
+    <div
+      style={{ '--width': width, '--height': height } as React.CSSProperties}
+      className={clsx(
+        className,
+        'relative aspect-[var(--width)/var(--height)] [--radius:var(--radius-xl)]',
+      )}
+    >
+      <div className="absolute -inset-(--padding) rounded-[calc(var(--radius)+var(--padding))] shadow-xs ring-1 ring-black/5 [--padding:--spacing(2)]" />
+      <Image alt=""
+        src={src}
+        className="h-full rounded-(--radius) shadow-2xl ring-1 ring-black/10"
+        width={width}
+        height={height}
+      />
+    </div>
+  )
+}
